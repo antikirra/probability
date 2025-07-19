f4529d52566c61c5abc85bf2b9baa7cdac61a4c3
antikirra <antikirra@gmail.com>
2025-07-19 15:06:47 +0700

    fix: add descriptive error messages to exceptions

--- probability.php
+++ probability.php
@@ -11,12 +11,16 @@ use InvalidArgumentException;
  */
 function probability($probability, $key = '')
 {
-    if (!is_float($probability) || !is_string($key)) {
-        throw new InvalidArgumentException();
+    if (!is_float($probability)) {
+        throw new InvalidArgumentException('Probability must be a float value');
+    }
+
+    if (!is_string($key)) {
+        throw new InvalidArgumentException('Key must be a string value');
     }
 
     if ($probability < 0.0 || $probability > 1.0) {
-        throw new InvalidArgumentException();
+        throw new InvalidArgumentException('Probability must be between 0.0 and 1.0, got: ' . $probability);
     }
 
     if ($probability === 0.0 || $probability <= 0.000001) {
@@ -27,8 +31,6 @@ function probability($probability, $key = '')
         return true;
     }
 
-    $value = $key === '' ? mt_rand(0, 4294967295) : hash('crc32b', $key);
-
-    return $value / 4294967295 <= $probability;
+    return ($key === '' ? mt_rand(0, 4294967295) : hash('crc32b', $key)) / 4294967295 <= $probability;
 }
 
