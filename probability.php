c418bf9674a0b929ec19290234c677f19d6210f1
antikirra <antikirra@gmail.com>
2025-07-19 14:40:23 +0700

    chore: remove obsolete function_exists checks

--- probability.php
+++ probability.php
@@ -4,33 +4,31 @@ namespace Antikirra;
 
 use InvalidArgumentException;
 
-
-if (!function_exists('Antikirra\\probability')) {
-    /**
-     * @param float $probability
-     * @param string $key
-     * @return bool
-     */
-    function probability($probability, $key = '')
-    {
-        if (!is_float($probability) || !is_string($key)) {
-            throw new InvalidArgumentException();
-        }
-
-        if ($probability < 0.0 || $probability > 1.0) {
-            throw new InvalidArgumentException();
-        }
-
-        if ($probability === 0.0 || $probability <= 0.000001) {
-            return false;
-        }
-
-        if ($probability === 1.0 || $probability >= 0.999999) {
-            return true;
-        }
-
-        $value = $key === '' ? mt_rand(0, 4294967295) : hash('crc32b', $key);
-
-        return $value / 4294967295 <= $probability;
+/**
+ * @param float $probability
+ * @param string $key
+ * @return bool
+ */
+function probability($probability, $key = '')
+{
+    if (!is_float($probability) || !is_string($key)) {
+        throw new InvalidArgumentException();
     }
+
+    if ($probability < 0.0 || $probability > 1.0) {
+        throw new InvalidArgumentException();
+    }
+
+    if ($probability === 0.0 || $probability <= 0.000001) {
+        return false;
+    }
+
+    if ($probability === 1.0 || $probability >= 0.999999) {
+        return true;
+    }
+
+    $value = $key === '' ? mt_rand(0, 4294967295) : hash('crc32b', $key);
+
+    return $value / 4294967295 <= $probability;
 }
+
